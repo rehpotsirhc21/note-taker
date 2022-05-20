b59fa499681f5bf82af144c22cddee835675db60
Christopher Langner <langnerc@icloud.com>
2022-05-20 13:29:11 -0500

    project completed minus bonus

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,21 +1,24 @@
-
-const router = require('express').Router();
-const {createNote} = require('../../lib/notes')
-const {notes} = require('../../db/db.json')
+const router = require("express").Router();
+const { createNote } = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
 
 //get the info from the notes array with a get api call
-router.get("/notes", (req, res)=> {
-    let results = notes
-  
-    res.json(results)
-})
+router.get("/notes", (req, res) => {
+  let results = notes;
+
+  res.json(results);
+});
 
 //update notes array with a new note
 
-router.post('/notes', (req,res)=> {
-    const results = notes;
-    const note = createNote(req.body,results )
-    res.json(results)
+router.post("/notes", (req, res) => {
+  const results = notes;
+  const note = createNote(req.body, results);
+  res.json(results);
+});
 
-})
-module.exports = router; 
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  
+});
+module.exports = router;
